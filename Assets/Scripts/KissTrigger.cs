6c380c177b4ff60180b6f47303a3cfa082cd900d
Thora Magnusdottir <70522885+thoraaslaug@users.noreply.github.com>
2025-04-20 11:54:25 +0200

    cameras

--- Assets/Scripts/KissTrigger.cs
+++ Assets/Scripts/KissTrigger.cs
@@ -1,15 +1,37 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Playables;
+using Cinemachine;
 
 public class KissTrigger : MonoBehaviour
 {
-    public PlayableDirector kissTimeline;
+    public AudioSource kissAudio;
+    public CinemachineVirtualCamera vcamKissZoom;
+
+    private bool hasPlayed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (!hasPlayed && other.CompareTag("Player"))
         {
-            kissTimeline.Play();
+            hasPlayed = true;
+            kissAudio.Play();
+
+            // Zoom in & slow mo
+            vcamKissZoom.Priority = 20;
+            Time.timeScale = 0.4f;
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+            StartCoroutine(ResetSceneAfterAudio(kissAudio.clip.length));
         }
+    }
+
+    IEnumerator ResetSceneAfterAudio(float duration)
+    {
+        yield return new WaitForSecondsRealtime(duration);
+
+        // Reset zoom and time
+        vcamKissZoom.Priority = 5;
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f;
     }
 }
