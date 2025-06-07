5a1b9ea0d2102d68507ceaf54eac27bb73efb762
Thora Magnusdottir <70522885+thoraaslaug@users.noreply.github.com>
2025-06-07 11:59:16 +0200

    final changes for now

--- Assets/Scripts/UI/TextHoverColor.cs
+++ Assets/Scripts/UI/TextHoverColor.cs
@@ -1,27 +1,47 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+using System.Collections;
 
 public class TextHoverColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public TMP_Text buttonText; // assign your text component
+    public TMP_Text buttonText;
     public Color normalColor = Color.white;
     public Color hoverColor = Color.yellow;
+    public float fadeDuration = 0.3f;
+
+    private Coroutine fadeCoroutine;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        buttonText.color = hoverColor;
+        StartFade(hoverColor);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        buttonText.color = normalColor;
+        StartFade(normalColor);
     }
 
-    void Start()
+    private void StartFade(Color targetColor)
     {
-        if (buttonText == null)
-            buttonText = GetComponentInChildren<TMP_Text>();
-        buttonText.color = normalColor;
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+
+        fadeCoroutine = StartCoroutine(FadeToColor(targetColor));
+    }
+
+    private IEnumerator FadeToColor(Color targetColor)
+    {
+        Color startColor = buttonText.color;
+        float elapsed = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            buttonText.color = Color.Lerp(startColor, targetColor, elapsed / fadeDuration);
+            yield return null;
+        }
+
+        buttonText.color = targetColor;
     }
 }
